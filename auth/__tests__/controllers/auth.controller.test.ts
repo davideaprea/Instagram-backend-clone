54831aa3b2002862a84a0fe4b79443b1478a05e0
daviaprea <apredav@gmail.com>
2024-10-29 18:58:29 +0100

    test completed

--- auth/__tests__/controllers/auth.controller.test.ts
+++ auth/__tests__/controllers/auth.controller.test.ts
@@ -2,13 +2,18 @@ import request from "supertest";
 import { app } from "../../src";
 
 it("registers a new user and returns just its username and full name, with a status of 201", async () => {
-    return request(app)
+    const res = await request(app)
         .post("/register")
         .send({
             username: "username",
             email: "email@gmail.com",
             password: "Password%2024",
             fullName: "Full Name"
-        })
-        .expect(201);
+        });
+    
+    expect(res.status).toBe(201);
+    expect(res.body).toEqual({
+        username: "username",
+        fullName: "Full Name"
+    });
 });
