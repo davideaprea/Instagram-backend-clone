3ae34547125bad4f5e4b36adb09e88169db3be27
daviaprea <apredav@gmail.com>
2024-11-08 16:35:45 +0100

    correctly receiving messages

--- profile/src/consumers/auth.consumer.ts
+++ profile/src/consumers/auth.consumer.ts
@@ -9,9 +9,15 @@ export const initAuthConsumer = async (): Promise<void> => {
     await consumer.subscribe({ topic: AuthTopics.USER_CREATE, fromBeginning: true });
     await consumer.run({
         eachMessage: async ({ topic, message }) => {
+            if(!message.value) return;
+
+            const stringMsg: string = message.value.toString();
+            const jsonMsg = JSON.parse(stringMsg);
+
+            console.log("Message received.", jsonMsg);
+
             switch (topic) {
                 case AuthTopics.USER_CREATE:
-                    console.log(message.value)
                     break;
             }
         },
