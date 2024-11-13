da3470ef7f25ab64766b87959fced69b6e50bbc0
daviaprea <apredav@gmail.com>
2024-11-13 22:09:09 +0100

    replicaset started

--- profile/src/server.ts
+++ profile/src/server.ts
@@ -4,7 +4,13 @@ import { initAuthConsumer } from "./consumers/auth.consumer";
 
 const init = async (): Promise<void> => {
     try {
-        await connect(process.env.LOCAL_DB_URL!);
+        await connect(process.env.LOCAL_DB_URL!, {
+            directConnection: true,
+            replicaSet: "rs0",
+            connectTimeoutMS: 40000,
+            serverSelectionTimeoutMS: 40000
+        });
+        
         console.log("Successully connected to the database.");
     } catch (e) {
         console.log("Couldn't connect to the database.", e);
