c00c894d3548f9e555b270b06301bd31e7a77080
daviaprea <apredav@gmail.com>
2024-11-15 15:02:24 +0100

    follow router validated

--- profile/src/routes/follow.router.ts
+++ profile/src/routes/follow.router.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
 import { handleAcceptFollow, handleFollow, handleUnfollow } from "../controllers/follow.controller";
+import { idSchema } from "../joi-schemas/id.schema";
 
 export const followRouter: Router = Router();
+
+followRouter.param("userId", async (req, res, next) => {
+    await idSchema.validateAsync(req.params.userId);
+    next();
+});
 
 followRouter
 .route("/follows/:userId")
